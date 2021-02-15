7aa46833204d5453eb924c2cfecd37e40cd736ea
timothymonyake <timothymonyake@gmail.com>
2021-02-15 17:27:43 +0200

    reconfigured tinymce editor

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,18 +9,9 @@
         </div>
         <div class="form-group">
             {{Form::label('body', 'Body')}}
-<<<<<<< HEAD
             {{Form::textarea('body', '',['class'=> 'form-control','id'=>'body', 'placeholder' => 'Post here..'])}}
         </div>
         {{Form::submit('Post', ['class'=>'btn btn-primary'])}}
     {!! Form::close() !!}
 
 @endsection
-=======
-            {{Form::textarea('body', '',['class'=> 'form-control', 'placeholder' => 'Post here..'])}}
-        </div>
-        {{Form::submit('Post', ['class'=>'btn btn-primary'])}}
-    {!! Form::close() !!}
-                 
-@endsection
->>>>>>> 1fe1cb0c6b04082d0da50d56109433277bdb1235
